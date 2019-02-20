6ef4b1a1ea38e9bbf8937ece21dabb070c436fe7
Jay Mohile <jamohile@gmail.com>
2019-02-19 19:53:24 -0500

    modified css to fix select on some browsers

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './utilities/store/reducers/root';
+import { configureAPI } from './utilities/network/network';
 
 const store = createStore(
     rootReducer,
     applyMiddleware(thunk)
 );
 
+configureAPI();
 
 ReactDOM.render(
     <Provider store={store}>
